95b4c41996444bab989a3e0276a1b73ec9ccd48d
ClaireQ <nnnnnkk1997@gmail.com>
2020-06-04 23:35:46 +0800

    fix bugs

---
+++ demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwEventGLFW.c" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwEventGLFW.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwEventGLUT.c" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwEventGLUT.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwEventSDL.c" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwEventSDL.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwEventSDL12.c" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwEventSDL12.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwEventSDL13.c" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwEventSDL13.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "TW_EXPORTS"
+  "TW_STATIC"
+  "_MACOSX"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks"
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/LoadOGL.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/LoadOGL.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/LoadOGLCore.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/LoadOGLCore.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwBar.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwBar.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwColors.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwColors.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwEventSFML.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwEventSFML.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwFonts.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwFonts.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwMgr.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwMgr.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwOpenGL.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwOpenGL.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwOpenGLCore.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwOpenGLCore.o"
+  "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/src/TwPrecomp.cpp" "/Users/claireqian/ENSTA/pre/PBD/demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/src/TwPrecomp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "TW_EXPORTS"
+  "TW_STATIC"
+  "_MACOSX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ demo_pbd/extern/AntTweakBar/build/CMakeFiles/AntTweakBar.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AntTweakBar.dir/src/LoadOGL.o"
+  "CMakeFiles/AntTweakBar.dir/src/LoadOGLCore.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwBar.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwColors.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwEventGLFW.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwEventGLUT.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwEventSDL.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwEventSDL12.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwEventSDL13.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwEventSFML.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwFonts.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwMgr.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwOpenGL.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwOpenGLCore.o"
+  "CMakeFiles/AntTweakBar.dir/src/TwPrecomp.o"
+  "libAntTweakBar.a"
+  "libAntTweakBar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/AntTweakBar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
