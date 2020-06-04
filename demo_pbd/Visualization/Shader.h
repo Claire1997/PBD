5cb3e798062adb37082675b746fe9c0a2efb0194
ClaireQ <nnnnnkk1997@gmail.com>
2020-06-04 22:32:37 +0800

    init

---
+++ demo_pbd/Visualization/Shader.h
@@ -0,0 +1,45 @@
+#ifndef __SHADER_H__
+#define __SHADER_H__
+
+#include <map>
+#include <string>
+#ifdef __APPLE__
+#include <OpenGL/gl.h> //OS x libs
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+namespace PBD
+{
+	class Shader
+	{
+	public:
+		Shader();
+		~Shader();
+
+		void compileShaderString(GLenum whichShader, const std::string &source);
+		void compileShaderFile(GLenum whichShader, const std::string &filename);
+		void createAndLinkProgram();
+		void addAttribute(const std::string &attribute);
+		void addUniform(const std::string &uniform);
+		bool isInitialized();
+
+		void begin();
+		void end();
+
+		//An indexer that returns the location of the attribute/uniform
+		GLuint getAttribute(const std::string &attribute);
+		GLuint getUniform(const std::string &uniform);
+
+	private:
+		bool m_initialized;
+		GLuint	m_program;
+		std::map<std::string, GLuint> m_attributes;
+		std::map<std::string, GLuint> m_uniforms;
+		GLuint m_shaders[3];	
+	};
+}
+
+#endif
